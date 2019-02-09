1aebe9c655294af89c5a4eefd5f57562a683425f
Ilya Gazman <gazman1986@gmail.com>
2019-02-08 22:53:41 -0500

    Minor cahnges

--- sample/src/main/java/com/gazman/diskcache/MainActivity.java
+++ sample/src/main/java/com/gazman/diskcache/MainActivity.java
@@ -3,9 +3,9 @@ package com.gazman.diskcache;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 
 import com.gazman.disk_cache.VIF;
